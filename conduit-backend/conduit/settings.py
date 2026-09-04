649fff23f57531b1eb91f72ad4b299a4d09279b2
WaldemarChorow <chorow.w@gmail.com>
2026-09-04 11:47:22 +0200

    fix: add CORS_ORIGIN_WHITELIST_DB to .env.example and settings.py for environment configuration

--- conduit-backend/conduit/settings.py
+++ conduit-backend/conduit/settings.py
@@ -133,10 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = (
-    '0.0.0.0:4000',
-    'localhost:4000',
-)
+CORS_ORIGIN_WHITELIST = os.getenv(
+    "CORS_ORIGIN_WHITELIST_DB", "http://167.235.51.233:8282"
+).split(",")
 
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
